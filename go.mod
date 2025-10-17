c1e6052d32a7e6ca40603b1cfad29040c31d14c7
Scott Ellis <scotte@warped.com>
2025-10-17 12:51:27 -0700

    Update dependencies. Update check for "oops, would have deleted the top level directory" with a better way (thanks Claude)

--- go.mod
+++ go.mod
@@ -1,8 +1,7 @@
 module github.com/scottesandiego/qbittorrent-filesync
 
-go 1.22.6
+go 1.25
 
-require (
-	github.com/NullpointerW/go-qbittorrent-apiv2 v0.0.16 // indirect
-	golang.org/x/net v0.9.0 // indirect
-)
+require github.com/NullpointerW/go-qbittorrent-apiv2 v0.0.17
+
+require golang.org/x/net v0.46.0 // indirect
--- qbittorrent-filesync.go
+++ qbittorrent-filesync.go
@@ -1,30 +1,38 @@
 package main
 
 import (
-	"os"
-	"fmt"
 	"flag"
-	"sort"
-	"slices"
+	"fmt"
+	"os"
 	"path/filepath"
+	"slices"
+	"sort"
+
 	qbt "github.com/NullpointerW/go-qbittorrent-apiv2"
 )
 
 func main() {
 	// Variables that'll be command line arguments
-	var hostname string	// IP or FQDN of the qBittorrent Web API server
-	var port string		// Port number of the qBittorent Web API
-	var username string	// Username for qBittorrent
-	var password string	// Password for qBittorrent
-	var filedir string	// Directory in the filesystem where the torrents live
+	var hostname string // IP or FQDN of the qBittorrent Web API server
+	var port string     // Port number of the qBittorent Web API
+	var username string // Username for qBittorrent
+	var password string // Password for qBittorrent
+	var filedir string  // Directory in the filesystem where the torrents live
 
 	flag.StringVar(&hostname, "hostname", "127.0.0.1", "IP or FQDN of the qBittorrent server")
 	flag.StringVar(&port, "port", "8080", "Port the qBittorrent Web API is running on")
 	flag.StringVar(&username, "username", "", "Username for qBittorrent")
 	flag.StringVar(&password, "password", "", "Password for qBittorrent")
-	flag.StringVar(&filedir, "directory", "/var/torrents/", "Directory in the filesystem where the torrents live with trailing slash")
+	flag.StringVar(&filedir, "directory", "/var/torrents/", "Directory in the filesystem where the torrents live")
 	dryrun := flag.Bool("dryrun", false, "Don't actually delete files, just print what would happen")
 	flag.Parse()
+
+	// Clean and validate the directory path
+	filedir = filepath.Clean(filedir)
+	if filedir == "." || filedir == "/" {
+		fmt.Println("ERROR: Invalid directory specified")
+		return
+	}
 
 	// Create a new qBitTorrent Web API client
 	client, err := qbt.NewCli("http://"+hostname+":"+port, username, password)
@@ -34,14 +42,14 @@ func main() {
 	}
 
 	// Get the list of completed torrents
-	torrents, err := client.TorrentList(qbt.Optional{"filter":"completed"})
+	torrents, err := client.TorrentList(qbt.Optional{"filter": "completed"})
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
 	// Build a slice of torrent names from the list of torrents
-	var torrentnames  []string
+	var torrentnames []string
 	for _, torrent := range torrents {
 		torrentnames = append(torrentnames, filepath.Base(torrent.ContentPath))
 	}
@@ -59,23 +67,25 @@ func main() {
 	for _, file := range files {
 		_, found := slices.BinarySearch(torrentnames, file.Name())
 		if !found {
-			// Super-duper extra check that file.Name() isn't null since os.RemoveAll will remove filedir otherwise!
-			if len(file.Name()) > 0 {
-				filepath := filedir + file.Name()
-				if *dryrun {
-					fmt.Printf("Dry-run, not deleting: %s\n", filepath)
-				} else {
-					fmt.Printf("Deleting unowned file: %s\n", filepath)
-					err := os.RemoveAll(filepath)
-					if err != nil {
-						fmt.Printf("ERROR deleting %s: %v\n", filepath, err)
-						continue
-					}
-				}
+			// Construct the full path safely
+			targetPath := filepath.Join(filedir, file.Name())
+
+			// Verify the path is still within the target directory (prevents path traversal)
+			if !filepath.HasPrefix(targetPath, filedir) {
+				fmt.Printf("ERROR: Suspicious path detected: %s\n", targetPath)
+				continue
+			}
+
+			if *dryrun {
+				fmt.Printf("Dry-run, not deleting: %s\n", targetPath)
 			} else {
-				fmt.Println("ERROR: Filename was empty!")
+				fmt.Printf("Deleting unowned file: %s\n", targetPath)
+				err := os.RemoveAll(targetPath)
+				if err != nil {
+					fmt.Printf("ERROR deleting %s: %v\n", targetPath, err)
+					continue
+				}
 			}
 		}
 	}
 }
-
